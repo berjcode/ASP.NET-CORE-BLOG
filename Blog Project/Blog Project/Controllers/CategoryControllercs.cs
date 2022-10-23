907f58b9873c5e51753edd43c972af0abe5303ab
Abdullah Balıkçı <abdblk2525@gmail.com>
2022-10-23 20:01:45 +0300

    Update

---
+++ Blog Project/Blog Project/Controllers/CategoryControllercs.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFrameWork;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog_Project.Controllers
+{
+    public class CategoryControllercs : Controller
+    {
+        //Bütün Metotlarıma erişim sağlıyorum.
+        CategoryManager _cm = new CategoryManager(new EfCategoryRepository());
+        public IActionResult Index()
+        {
+            var values = _cm.GetList();
+            return View(values);
+        }
+    }
+}
