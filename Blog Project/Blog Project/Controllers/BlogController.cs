b9fbe8617593d0c9f0022a4dfe2692a7009e79fd
Abdullah Balıkçı <abdblk2525@gmail.com>
2022-10-24 10:11:10 +0300

    Blog List

---
+++ Blog Project/Blog Project/Controllers/BlogController.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFrameWork;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog_Project.Controllers
+{
+    public class BlogController : Controller
+    {
+        BlogManager _bm = new BlogManager(new EfBlogRepository());
+        public IActionResult Index()
+        {
+            var values = _bm.GetBlogListWithCategory();
+            return View(values);
+        }
+    }
+}
